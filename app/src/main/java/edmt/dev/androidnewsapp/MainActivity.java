be06a91850c0223a332fa8929618816b429d2993
ammy4321 <amrit.amu98@gmail.com>
2018-11-14 23:41:07 +0530

    Initial commit

---
+++ app/src/main/java/edmt/dev/androidnewsapp/MainActivity.java
@@ -0,0 +1,167 @@
+package edmt.dev.androidnewsapp;
+
+import android.app.ActionBar;
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.gson.Gson;
+
+import dmax.dialog.SpotsDialog;
+import edmt.dev.androidnewsapp.Adapter.ListSourceAdapter;
+import edmt.dev.androidnewsapp.Common.Common;
+import edmt.dev.androidnewsapp.Interface.NewsService;
+import edmt.dev.androidnewsapp.Model.WebSite;
+import io.paperdb.Paper;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView listWebsite;
+    RecyclerView.LayoutManager layoutManager;
+    NewsService mService;
+    ListSourceAdapter adapter;
+    AlertDialog dialog;
+    SwipeRefreshLayout swipeLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        swipeLayout = (SwipeRefreshLayout)findViewById(R.id.swipeRefresh);
+        listWebsite = (RecyclerView)findViewById(R.id.list_source);
+        ActionBar ab= getActionBar();
+
+
+        //Init cache
+        Paper.init(this);
+
+        //Init Service
+        mService = Common.getNewsService();
+
+        //Init View
+
+        swipeLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                loadWebsiteSource(true);
+            }
+        });
+
+
+        listWebsite.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        listWebsite.setLayoutManager(layoutManager);
+
+        dialog = new SpotsDialog(this);
+
+        loadWebsiteSource(false);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+         getMenuInflater().inflate(R.menu.commonmenu,menu);
+        return super.onCreateOptionsMenu(menu);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.signo) {
+            AuthUI.getInstance().signOut(this).addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                    Log.d("AUTH", "USER LOGGED OUT");
+                    startActivity(new Intent(MainActivity.this, login.class));
+                    finish();
+                }
+            });
+        }
+        Intent intent=this.getIntent();
+        if(intent !=null) {
+            // do something you want
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void loadWebsiteSource(boolean isRefreshed) {
+        if(!isRefreshed)
+        {
+
+            String cache = Paper.book().read("cache");
+            if(cache != null && !cache.isEmpty() && !cache.equals("null")) // If have cache
+            {
+                WebSite website = new Gson().fromJson(cache,WebSite.class); // Convert cache from Json to Object
+                adapter = new ListSourceAdapter(getBaseContext(),website);
+                adapter.notifyDataSetChanged();
+                listWebsite.setAdapter(adapter);
+            }
+            else // If not have cache
+            {
+                dialog.show();
+                //Fetch new data
+                mService.getSources().enqueue(new Callback<WebSite>() {
+                    @Override
+                    public void onResponse(Call<WebSite> call, Response<WebSite> response) {
+                        adapter  = new ListSourceAdapter(getBaseContext(),response.body());
+                        adapter.notifyDataSetChanged();
+                        listWebsite.setAdapter(adapter);
+
+                        //Save to cache
+                        Paper.book().write("cache",new Gson().toJson(response.body()));
+
+                        dialog.dismiss();
+
+                    }
+
+                    @Override
+                    public void onFailure(Call<WebSite> call, Throwable t) {
+
+                    }
+                });
+            }
+        }
+        else // If from Swipe to Refresh
+        {
+
+            swipeLayout.setRefreshing(true);
+            //Fetch new data
+            mService.getSources().enqueue(new Callback<WebSite>() {
+                @Override
+                public void onResponse(Call<WebSite>   call, Response<WebSite> response) {
+                    adapter  = new ListSourceAdapter(getBaseContext(),response.body());
+                    adapter.notifyDataSetChanged();
+                    listWebsite.setAdapter(adapter);
+
+                    //Save to cache
+                    Paper.book().write("cache",new Gson().toJson(response.body()));
+
+                    //Dismiss refresh progressring
+                    swipeLayout.setRefreshing(false);
+                }
+
+                @Override
+                public void onFailure(Call<WebSite> call, Throwable t) {
+
+                }
+            });
+
+        }
+
+    }
+}
---
+++ app/src/main/java/edmt/dev/androidnewsapp/login.java
@@ -0,0 +1,77 @@
+package edmt.dev.androidnewsapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class login extends AppCompatActivity {
+    private static final int RC_SIGN_IN = 0;
+    private FirebaseAuth auth;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        auth = FirebaseAuth.getInstance();
+
+        Intent i = new Intent(this, login.class);
+        startActivity(i);
+        if (auth.getCurrentUser() != null) {
+            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+            startActivity(new Intent(login.this, MainActivity.class));
+
+        } else {
+            List<AuthUI.IdpConfig> providers = Arrays.asList(
+                    new AuthUI.IdpConfig.EmailBuilder().build(),
+                    new AuthUI.IdpConfig.GoogleBuilder().build(),
+                    new AuthUI.IdpConfig.FacebookBuilder().build());
+
+            startActivityForResult(
+                    AuthUI.getInstance()
+                            .createSignInIntentBuilder()
+                            .setAvailableProviders(providers)
+                            .build(),
+                    RC_SIGN_IN);
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            if (resultCode == RESULT_OK) {
+                // Successfully signed in
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                Intent i = new Intent(login.this, MainActivity.class);
+                startActivity(i);
+
+                if (user != null) {
+
+                }
+
+                else{
+                    // Sign in failed.
+
+                    Log.d("AUTH", "Sign in Failed");
+                    finish();
+                }
+            }
+        }
+    }
+}
